0b501ab94cb583da08170fc851b7606157a04f92
JBayona <jordavids_22@hotmail.com>
2020-07-23 18:25:09 -0700

    gRPC Unary API.

---
+++ go-grpc-course/greet/green_client/client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/go-grpc-course/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+
+	defer cc.Close() // Call at the very end
+
+	c := greetpb.NewGreetServiceClient(cc)
+	// fmt.Printf("Created client: %f", c)
+
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a Unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Jorge",
+			LastName:  "Bayona",
+		},
+	}
+	fmt.Printf("Request: %v", req)
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling Greet RPC: %v", err)
+	}
+	log.Printf("Response from Greet: %v", res.Result)
+}
---
+++ go-grpc-course/greet/greet_server/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/go-grpc-course/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+// type server struct{i int}
+
+// This is a structure that can only be called by the "server" structure
+// func (s *server)
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v", req)
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
+
+func main() {
+	fmt.Println("Hello World")
+
+	// 50051 defult port for gRPC
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	// Register new service
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
