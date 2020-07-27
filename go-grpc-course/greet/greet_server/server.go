633e9dc2aee44105abc6ce945c38e91f82ded4d5
JBayona <jordavids_22@hotmail.com>
2020-07-27 15:25:18 -0700

    Implement Server Streaming API.

--- go-grpc-course/greet/greet_server/server.go
+++ go-grpc-course/greet/greet_server/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
 	"github.com/go-grpc-udemy-course/go-grpc-course/greet/greetpb"
 	"google.golang.org/grpc"
@@ -24,6 +26,20 @@ func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.G
 		Result: result,
 	}
 	return res, nil
+}
+
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+	fmt.Printf("GreetManyTimes function was invoked with %v\n", req)
+	firstName := req.GetGreeting().GetFirstName()
+	for i := 0; i < 10; i++ {
+		result := "Result " + firstName + " number " + strconv.Itoa(i)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+		stream.Send(res)
+		time.Sleep(1000 * time.Millisecond)
+	}
+	return nil
 }
 
 func (*server) AdditionOperation(ctx context.Context, req *greetpb.SumRequest) (*greetpb.SumResponse, error) {
